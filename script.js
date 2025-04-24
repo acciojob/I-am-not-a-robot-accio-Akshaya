92df353929bea2acce1ea45b1635fbb8963b5a4e
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-04-24 00:59:14 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
-//your code here
+const imageClass = ["img1","img2","img3","img4","img5"];
+const tilesContainer = document.getElementById("tiles");
+const resetBtn = document.getElementById("reset");
+const verifyBtn = document.getElementById("verify");
+const message = document.getElementById("para");
+
+let selectedImages = [];
+
+function shuffle(arr){
+	for(let i = arr.length - 1; i > 0; i--){
+			const j = Math.floor(Math.random() * (i + 1));
+		    [arr[i],arr[j]] = [arr[j],arr[i]];
+	}
+}
+
+function loadImages(){
+	tilesContainer.innerHTML = "";
+	message.innerText = "";
+	verifyBtn.style.display = "none";
+	resetBtn.style.display = "none";
+	selectedImages = [];
+
+	const duplicateIndex = Math.floor(Math.random() * imageClasses.length);
+	const images = [...imageClasses, imageclasses[duplicateIndex]];
+	shuffle(images);
+
+	images.forEach((cls) => {
+    const img = document.createElement("img");
+    img.classList.add(cls);
+    img.dataset.cls = cls;
+    img.addEventListener("click", () => handleImageClick(img));
+    tilesContainer.appendChild(img);
+  });
+	
+}
+
+function handleImageClick(img) {
+  if (selectedImages.includes(img) || selectedImages.length >= 2) return;
+
+  img.classList.add("selected");
+  selectedImages.push(img);
+  resetBtn.style.display = "inline-block";
+
+  if (selectedImages.length === 2) {
+    verifyBtn.style.display = "inline-block";
+  }
+}
+
+// Handle reset
+resetBtn.addEventListener("click", () => {
+  loadImages();
+});
+
+// Handle verification
+verifyBtn.addEventListener("click", () => {
+  verifyBtn.style.display = "none";
+
+  const [img1, img2] = selectedImages;
+  if (img1.dataset.cls === img2.dataset.cls) {
+    message.innerText = "You are a human. Congratulations!";
+  } else {
+    message.innerText = "We can't verify you as a human. You selected the non-identical tiles.";
+  }
+});
+
+window.onload = loadImages;
